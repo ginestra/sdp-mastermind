b4140273dd61d4176de6b1621e5e87a513a6f9db
Alun Davey <hey@4lun.io>
2018-04-28 16:24:57 +0100

    Implement dependency injection

--- src/Factory.kt
+++ src/Factory.kt
@@ -1,3 +1,9 @@
+import com.github.salomonbrys.kodein.*
+
+val kodein = Kodein {
+    bind<Game>() with factory { showCode: Boolean -> GameImpl(showCode) }
+}
+
 object Factory {
-    fun getInstance(c: Class<*>, b: Boolean?): Game = TODO()
+    fun getInstance(c: Class<*>, b: Boolean?): Game = kodein.with(b).instance()
 }
--- src/MastermindDriver.kt
+++ src/MastermindDriver.kt
@@ -3,10 +3,8 @@ object MastermindDriver {
     // Example - change as you need to...
     @JvmStatic
     fun main(args: Array<String>) {
-        var g = Factory.getInstance(Game::class.java, true)
-        g.runGames()
 
-        g = Factory.getInstance(Game::class.java, false)
+        var g = Factory.getInstance(Game::class.java, true)
         g.runGames()
     }
 }
